ed433e5ef2333baa8ca073761f0119d208df6f0c
Guilherme Ferreira <90099945+ffguiff@users.noreply.github.com>
2026-03-27 23:01:38 -0300

    Limpeza de dados em formato JSON
    Algumas balanças geram dados em formato JSON, realizei a limpeza do dados brutos gerados pela balança e formatei para CSV para posteriormente carrega-lo no power BI para futuras analises.

--- limpeza_analise.py
+++ limpeza_analise.py
@@ -1,37 +1,37 @@
 import pandas as pd
+import json
 
-#carrega o arquivo csv
-df = pd.read_csv('balanca.csv')
+#carregar o aquivo JSON
 
-# remover linhas com erro de leitura
-df  = df[df['status_sensor'] != 'erro_leitura']
+with open('pesagens_sensor.json') as f:
+    data = json.load(f)
 
-'''O simbolo != significa que o pandas vai olhar para o status 
-e criar um linha de verdadeiro ou falso. se falso será igual a erro.
-Quando colocado o filtro df[]estou dizendo para o pandas pegar somente as linhas onde a condição é verdadeira 
-Ao colocar do df['status_sensor'] dentro de outro df[] estou dizendo para o pandas salvar a nova tabela.'''
+# "achatar" o JSON para virar uma tabela comum
+df = pd.json_normalize(data)
 
-# padronizar para kg
+# print(df.columns)
+
+#limpar linhas com status diferente de "sucesso"
+df = df[df['status'] == 'sucesso']
+
+#Padronizador de pesagens
 def padronizar_peso(row):
-    if row['unidade'] == 'g':
-        return row ['peso_lido']/1000
-    return row['peso_lido']
+    if row['dados_tecnicos.unidade'] == 'g':
+        return row ['dados_tecnicos.valor']/1000
+    return row['dados_tecnicos.valor']
 
-df['peso_final_kg'] = df.apply(padronizar_peso, axis = 1)
+df['peso_final_kg'] = df.apply(padronizar_peso, axis=1)
 
-'''row = linha que será analisada 
-quando estiver na linha x e a unidade for g ela será dividida por 1000 para padronizar para kg
-apply ira executar a função padronizar_peso para cada linha. axis =1 indica que a função deve ser aplicada na horizontal.
-é criada uma coluna nova chamada peso_final_kg para armazenar os valores padronizados.'''
+print("Tabala limpa e padronizada:")
+colunas_selecionadas = [
+    '_comment',
+    'status',
+    'dados_tecnicos.lote',
+    'dados_tecnicos.produto',
+    'peso_final_kg'
+]
+print(df[colunas_selecionadas])
 
-# 3 media de peso por lote
-
-media_lote = df.groupby(['lote_id','produto'])['peso_final_kg'].mean()
-
-print("dados processados para insights:")
-print(f" Média de peso por lote:\n{media_lote}")
-print(df[['timestamp', 'lote_id', 'peso_final_kg', 'status_sensor']])
-
-'''mean() é uma função do pandas que calcula a média
-a variavel media_lote se torna meio que uma matriz onde os dados estão agrupados, ela 
-é definida dessa forma porque o groupby cria um objeto de grupo.'''
+# Exportar para CSV (mais leve e rápido para o Power BI)
+# exporção do arquivo limpo para o Power BI possibilitando a análise e visualização dos dados de pesagem
+df.to_csv('pesagens_prontas_powerbi.csv', index=False, sep=';', encoding='utf-8-sig')
